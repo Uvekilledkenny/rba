d2e61bdba3dbe6b283b1ea68dbec9c4f15fc56bb
uvekilledkenny <dimitri.tavan@gmail.com>
2017-04-27 23:33:21 +0200

    Io_reg Interrupts Test

--- arm/src/lib.rs
+++ arm/src/lib.rs
@@ -6,10 +6,20 @@ extern crate baselib;
 
 use baselib::*;
 
+type CFunc = extern "C" fn();
+
 #[allow(non_snake_case)]
 #[no_mangle]
 #[link_section=".iwram"]
-pub extern "C" fn InterruptProcess() {
+pub extern "C" fn VBlank() {
     hw::write_regifbios(1);
     hw::write_regif(1);
 }
+
+pub extern "C" fn default() {}
+
+#[no_mangle]
+#[link_section = ".iwram"]
+pub static IntrTable: [CFunc; 13] = 
+    [VBlank, default, default, default, default, default, 
+    default, default, default, default, default, default, default];
